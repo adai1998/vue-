20cbc7d7b4269f2b37aa9086252ae53326ab4135
阿呆 <adai1998@163.com>
2020-12-22 16:28:01 +0800

    tijiao

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
-  <div>App...</div>
+  <div>
+    <Header></Header>
+    <router-view></router-view>
+    <Footer v-show="!this.$route.meta.isHideFooter"></Footer>
+  </div>
 </template>
 
 <script>
+import Header from './components/Header'
+import Footer from './components/Footer'
 export default {
   name: '',
+  components:{
+    Header,
+    Footer
+  }
 }
 </script>
 
---
+++ src/views/Search.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>Search:{{keyword}}</div>
+</template>
+
+<script>
+export default {
+    name: '',
+    props:['keyword']
+}
+</script>
+
+<style scoped>
+
+</style>
